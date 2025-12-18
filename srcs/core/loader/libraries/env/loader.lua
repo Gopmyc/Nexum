da43510c11fae63bc7e9819047f98a0ffc425e0c
Gopmyc <hoyaux.gaspard@gmail.com>
2025-12-18 10:52:16 +0100

    refactor(srcs/core/loader/libraries/env/loader.lua): ♻️ - Updated core base systems (loader.lua)

--- srcs/core/loader/libraries/env/loader.lua
+++ srcs/core/loader/libraries/env/loader.lua
@@ -1,4 +1,9 @@
-function LIBRARY:ResolveFileSource(sFileSource)
+LIBRARY.ENTRY_POINT	= {
+	SERVER	= "init.lua",
+	CLIENT	= "init.lua",
+}
+
+function LIBRARY:ResolveFileSource(sFileSource, bIsSubFile)
 	local bIsFile	= lovr.filesystem.isFile(sFileSource)
 	local bIsDir	= lovr.filesystem.isDirectory(sFileSource)
 
@@ -7,7 +12,7 @@ function LIBRARY:ResolveFileSource(sFileSource)
 	end
 
 	if bIsFile then
-		return sFileSource
+		return sFileSource, bIsSubFile or false
 	end
 
 	sFileSource		= sFileSource:sub(-1) ~= "/" and sFileSource .. "/" or sFileSource
@@ -18,21 +23,28 @@ function LIBRARY:ResolveFileSource(sFileSource)
 	local sFallback	= sFileSource .. (SERVER and "server/" or "client/")
 	MsgC(Color(241, 196, 15), "[WARNING][ENV-RESSOURCES] main file 'init.lua' not found, switch to : " .. sFallback)
 
-	return self:ResolveFileSource(sFallback .. "init.lua")
+	return self:ResolveFileSource(sFallback .. "init.lua", true)
 end
 
-function LIBRARY:LoadSubEnvironments(sBasePath, tSandEnv, sAccessPoint, tFileArgs)
-	local sClient		= sBasePath .. "client/cl_init.lua"
-	local sServer		= sBasePath .. "server/sv_init.lua"
+function LIBRARY:LoadSubEnvironments(sBasePath, tSandEnv, sAccessPoint, tFileArgs, tCapabilities)
+	local sClient		= sBasePath .. "client/" .. self.ENTRY_POINT.CLIENT
+	local sServer		= sBasePath .. "server/" .. self.ENTRY_POINT.SERVER
+
+	local tSharedEnv	= table.Copy(tSandEnv, true)
+
+	if istable(tSandEnv[sAccessPoint]) and istable(tSandEnv[sAccessPoint].LIBRARIES) then
+		tSharedEnv[sAccessPoint]				= tSharedEnv[sAccessPoint] or {}
+		tSharedEnv[sAccessPoint].LIBRARIES		= tSandEnv[sAccessPoint].LIBRARIES
+	end
 
 	local tServerEnv	= SERVER and
 		lovr.filesystem.isFile(sServer) and
-		self:Load(sServer, tSandEnv, sAccessPoint, tFileArgs) or
+		self:Load(sServer, tSharedEnv, sAccessPoint, tFileArgs, false, tCapabilities, true) or
 		nil
 
 	local tClientEnv	= CLIENT and
 		lovr.filesystem.isFile(sClient) and
-		self:Load(sClient, tSandEnv, sAccessPoint, tFileArgs) or
+		self:Load(sClient, tSharedEnv, sAccessPoint, tFileArgs, false, tCapabilities, true) or
 		nil
 
 	return tServerEnv, tClientEnv
@@ -52,15 +64,24 @@ function LIBRARY:ExecuteChunk(sFileSource, tEnv)
 	return true
 end
 
-function LIBRARY:MergeSubEnvironment(tMainEnv, tSubEnv)
-	for sKey, vValue in pairs(tSubEnv or {}) do
-		if sKey ~= "__PATH" and sKey ~= "__NAME" and sKey ~= "LIBRARIES" then
-			tMainEnv[sKey] = vValue
+function LIBRARY:MergeSubEnvironments(tMainEnv, ...)
+	for iID, tSubEnv in pairs({...}) do
+		if not istable(tSubEnv) then
+			MsgC(Color(241, 196, 15), "[WARNING][ENV-RESSOURCES] Failed to merge sub-environment ID : '" .. iID .. "' : invalid table")
+			goto continue
 		end
+
+		for sKey, vValue in pairs(tSubEnv or {}) do
+			if sKey ~= "__PATH" and sKey ~= "__NAME" and sKey ~= "LIBRARIES" then
+				tMainEnv[sKey] = vValue
+			end
+		end
+
+		::continue::
 	end
 end
 
-function LIBRARY:Load(sFileSource, tSandEnv, sAccessPoint, tFileArgs, bLoadSubFolders, tCapabilities)
+function LIBRARY:Load(sFileSource, tSandEnv, sAccessPoint, tFileArgs, bLoadSubFolders, tCapabilities, bNotLoadLibraries)
 	assert(isstring(sFileSource),					"[ENV-RESSOURCES] FileSource must be a string (#1)")
 	assert(istable(tSandEnv),						"[ENV-RESSOURCES] ENV must be a table (#2)")
 	assert(isstring(sAccessPoint),					"[ENV-RESSOURCES] AccessPoint must be a string (#3)")
@@ -71,20 +92,20 @@ function LIBRARY:Load(sFileSource, tSandEnv, sAccessPoint, tFileArgs, bLoadSubFo
 		return MsgC(Color(241, 196, 15), "[WARNING] 'Load' method in loader library : 'ENV_LOADER', failed. 'ENV_BUILDER' library not found")
 	end
 
-	local sResolved	= self:ResolveFileSource(sFileSource)
+	local sResolved		= self:ResolveFileSource(sFileSource)
 	if not sResolved then
-		return nil
+		return MsgC(Color(241, 196, 15), "[WARNING][ENV-RESSOURCES] Failed to resolve file source: " .. sFileSource)
 	end
 
 	local tServerEnv, tClientEnv;
 	if bLoadSubFolders and sResolved:sub(-8) == "init.lua" then
-		local sBasePath = sResolved:match("^(.*[/\\])")
-		tServerEnv, tClientEnv = self:LoadSubEnvironments(sBasePath, tSandEnv, sAccessPoint, tFileArgs)
+		local sBasePath			= sResolved:match("^(.*[/\\])")
+		tServerEnv, tClientEnv	= self:LoadSubEnvironments(sBasePath, tSandEnv, sAccessPoint, tFileArgs, tCapabilities)
 	end
 
-	local tEnv		= tEnvBuilder:BuildEnvironment(sResolved, tSandEnv, sAccessPoint, tFileArgs, tCapabilities)
+	local tEnv		= tEnvBuilder:BuildEnvironment(sResolved, tSandEnv, sAccessPoint, tFileArgs, tCapabilities, not bNotLoadLibraries)
 	local tSubEnv	= (SERVER and tServerEnv) or (CLIENT and tClientEnv) or {}
-	self:MergeSubEnvironment(tEnv[sAccessPoint], tSubEnv)
+	self:MergeSubEnvironments(tEnv[sAccessPoint], tSubEnv)
 
 	self:ExecuteChunk(sResolved, tEnv)
 
