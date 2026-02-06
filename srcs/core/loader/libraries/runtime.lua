55336c970b5e76ebc42b3decf09e75f3f646d089
Gopmyc <hoyaux.gaspard@gmail.com>
2026-02-06 16:16:25 +0100

    refactor(srcs/core/loader/libraries/runtime.lua): ♻️ - Added core base systems (runtime.lua)

---
+++ srcs/core/loader/libraries/runtime.lua
@@ -0,0 +1,149 @@
+LIBRARY.TEST = "12345"
+
+LIBRARY.INSTANCES		= {}
+LIBRARY.UPDATE_PIPELINE	= {}
+LIBRARY.DRAW_PIPELINE	= {}
+
+function LIBRARY:SetRuntimeConfig(tRuntimeConfig)
+	assert(istable(tRuntimeConfig), "Runtime configuration must be a table")
+	self.RUNTIME_CONFIG = tRuntimeConfig
+end
+
+function LIBRARY:Instantiate(sFileName)
+	assert(isstring(sFileName), "FileName must be a string")
+
+	local tLibRess	= assert(self:GetLibrary("RESSOURCES"), "'RESSOURCES' library is required")
+	local tClass	= assert(tLibRess:GetScript(sFileName), "File '" .. sFileName .. "' not found in RESSOURCES")
+
+	local bSuccess, tInstance = xpcall(
+		function()
+			return tClass:Initialize()
+		end,
+		function(sErr)
+			return MsgC(Color(231, 76, 60), "[ERROR] " .. sErr .. "\n" .. debug.traceback())
+		end
+	)
+
+	if not bSuccess or not istable(tInstance) then
+		return MsgC(Color(231, 76, 60), "[ERROR] Failed to instantiate '" .. sFileName .. "'\n")
+	end
+
+	self.INSTANCES[#self.INSTANCES + 1] = tInstance
+
+	self:RegisterInstance(tInstance)
+
+	return tInstance
+end
+
+function LIBRARY:RegisterInstance(tInstance)
+	assert(istable(tInstance),	"Instance must be a table")
+	assert(self.RUNTIME_CONFIG,	"Runtime configuration not set")
+
+	if isstring(tInstance.STAGE_UPDATE) then
+		local tStages	= self.RUNTIME_CONFIG.UPDATE
+		local nStage	= tStages[tInstance.STAGE_UPDATE]
+
+		assert(isnumber(nStage), "Invalid UPDATE stage '" .. tInstance.STAGE_UPDATE .. "'")
+
+		local nOrder	= isnumber(tInstance.ORDER_UPDATE) and tInstance.ORDER_UPDATE or 0
+		local nPriority	= nStage * 1000 + nOrder
+
+		self.UPDATE_PIPELINE[#self.UPDATE_PIPELINE + 1] = {
+			tInstance	= tInstance,
+			nPriority	= nPriority,
+			bEnabled	= true,
+		}
+	end
+
+	if isstring(tInstance.STAGE_DRAW) then
+		local tStages	= self.RUNTIME_CONFIG.DRAW
+		local nStage	= tStages[tInstance.STAGE_DRAW]
+
+		assert(isnumber(nStage), "Invalid DRAW stage '" .. tInstance.STAGE_DRAW .. "'")
+
+		local nOrder	= isnumber(tInstance.ORDER_DRAW) and tInstance.ORDER_DRAW or 0
+		local nPriority	= nStage * 1000 + nOrder
+
+		self.DRAW_PIPELINE[#self.DRAW_PIPELINE + 1] = {
+			tInstance	= tInstance,
+			nPriority	= nPriority,
+			bEnabled	= true,
+		}
+	end
+
+	table.sort(self.UPDATE_PIPELINE, function(a, b)
+		return a.nPriority < b.nPriority
+	end)
+
+	table.sort(self.DRAW_PIPELINE, function(a, b)
+		return a.nPriority < b.nPriority
+	end)
+end
+
+function LIBRARY:Update(...)
+	local tArgs	= {...}
+
+	for i = 1, #self.UPDATE_PIPELINE do
+		local tNode	= self.UPDATE_PIPELINE[i]
+
+		if not tNode.bEnabled then goto continue end
+
+		local tInst		= tNode.tInstance
+		if not isfunction(tInst.Update) then goto continue end
+
+		local bSuccess	= xpcall(
+			function()
+				return tInst:Update(unpack(tArgs))
+			end,
+			function(sErr)
+				MsgC(
+					Color(231, 76, 60),
+					"[RUNTIME][UPDATE][DISABLED] ",
+					tostring(tInst),
+					"\n",
+					sErr,
+					"\n",
+					debug.traceback()
+				)
+			end
+		)
+
+		if not bSuccess then tNode.bEnabled=false; end
+
+		::continue::
+	end
+end
+
+function LIBRARY:Draw(...)
+	local tArgs	= {...}
+	
+	for i = 1, #self.DRAW_PIPELINE do
+		local tNode	= self.DRAW_PIPELINE[i]
+
+		if not tNode.bEnabled then goto continue end
+		
+		local tInst		= tNode.tInstance
+		if not isfunction(tInst.Draw) then goto continue end
+
+		local bSuccess	= xpcall(
+			function()
+				return tInst:Draw(unpack(tArgs))
+			end,
+			function(sErr)
+				MsgC(
+					Color(231, 76, 60),
+					"[RUNTIME][DRAW][DISABLED] ",
+					tostring(tInst),
+					"\n",
+					sErr,
+					"\n",
+					debug.traceback()
+				)
+			end
+		)
+
+		if not bSuccess then tNode.bEnabled=false; end
+
+		::continue::
+	end
+end
