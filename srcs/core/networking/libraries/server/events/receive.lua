febb462ac8db892c4182626423e9be3580eebaf8
Gopmyc <hoyaux.gaspard@gmail.com>
2026-02-11 02:25:39 +0100

    refactor(srcs/core/networking/libraries/server/events/receive.lua): ♻️ - update core logic (receive.lua)

--- srcs/core/networking/libraries/server/events/receive.lua
+++ srcs/core/networking/libraries/server/events/receive.lua
@@ -1,20 +1,17 @@
-function LIBRARY:Call(tServer, tEvent) -- tData : {sID, ...}
+function LIBRARY:Call(tServer, tEvent)
 	local sID		= tostring(tEvent.udPeer:connect_id())
 	local udPeer	= tServer:IsValidClient(sID)
 	
 	if not udPeer then
-		return MsgC(Color(231, 76, 60), "Unregister Client [ID : " .. sID .. "] attempted to send message : " .. tostring(tEvent.tData))
+		return MsgC(Color(231, 76, 60), "Unregister Client [ID : " .. sID .. "] attempted to send message : " .. tostring(tEvent.Data))
 	end
 
-	---- pcall all steps ----
-	-- // TODO : Decrypt data...
-	-- // TODO : Uncompress data...
-	-- // TODO : JSON to Table...
+	local sPacketID, Content	= tServer.CODEC:Decode(tEvent.Data)
 	
-	if not tServer:IsValidMessage(tData[1]) then
-		return MsgC(Color(231, 76, 60), "Client [ID : " .. sID .. "] attempted to send an undeclared message : " .. tostring(tData[1]))
+	if not tServer:IsValidMessage(sPacketID) then
+		return MsgC(Color(231, 76, 60), "Client [ID : " .. sID .. "] attempted to send an undeclared message : " .. tostring(sPacketID))
 	end
 
 	tServer.CLIENTS[sID][2]	=	os.time()
-	tServer.HOOKS:CallHook(tData[1], tData[2])
+	tServer.HOOKS:CallHook(sPacketID, Content)
 end
