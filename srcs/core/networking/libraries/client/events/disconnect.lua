5565d53870bc0f59ac44b7363482cc6f120c674f
Gopmyc <hoyaux.gaspard@gmail.com>
2026-02-12 04:32:36 +0100

    refactor(srcs/core/networking/libraries/client/events/disconnect.lua): ♻️ - update core logic (disconnect.lua)

--- srcs/core/networking/libraries/client/events/disconnect.lua
+++ srcs/core/networking/libraries/client/events/disconnect.lua
@@ -1,8 +1,8 @@
-function LIBRARY:Call(tServer, tEvent)
+function LIBRARY:Call(tClient, tEvent)
 	local sID	= tostring(tEvent.peer:connect_id())
 
 	MsgC(Color(241, 196, 15), "Disconnected from server [ID : " .. sID .. "] : " .. tostring(tEvent.peer) .. "[" .. os.date("%Y-%m-%d %H:%M:%S") .. "]")
 
-	tServer.PEER:disconnect()
-	tServer.PEER = nil
+	tClient.PEER:disconnect()
+	tClient.PEER = nil
 end
