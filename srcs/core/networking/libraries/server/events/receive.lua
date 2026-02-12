33f873064dd4eb8e3a9c97477c27b30339a7a9d4
Gopmyc <hoyaux.gaspard@gmail.com>
2026-02-12 04:32:38 +0100

    refactor(srcs/core/networking/libraries/server/events/receive.lua): ♻️ - update core logic (receive.lua)

--- srcs/core/networking/libraries/server/events/receive.lua
+++ srcs/core/networking/libraries/server/events/receive.lua
@@ -1,15 +1,21 @@
 function LIBRARY:Call(tServer, tEvent)
 	local sID		= tostring(tEvent.udPeer:connect_id())
-	local udPeer	= tServer:IsValidClient(sID)
+	local tPeer		= tServer:IsValidClient(sID)
 	
-	if not udPeer then
+	if not tPeer then
 		return MsgC(Color(231, 76, 60), "Unregister Client [ID : " .. sID .. "] attempted to send message : " .. tostring(tEvent.Data))
 	end
 
 	local sPacketID, Content	= tServer.CODEC:Decode(tEvent.Data)
 	
 	if not tServer:IsValidMessage(sPacketID) then
-		return MsgC(Color(231, 76, 60), "Client [ID : " .. sID .. "] attempted to send an undeclared message : " .. tostring(sPacketID))
+		return tServer.EVENTS:Call(tServer, {
+			type	= "unhandled",
+			peer	= tEvent.udPeer,
+			data	= { sPacketID, Content },
+			channel	= nil,
+			flag	= nil,
+		})
 	end
 
 	tServer.CLIENTS[sID][2]	=	os.time()
