3b589122247b004575985f2224d0092b6af24728
Gopmyc <hoyaux.gaspard@gmail.com>
2026-02-12 04:32:38 +0100

    refactor(srcs/core/networking/libraries/server/events/send.lua): ♻️ - update core logic (send.lua)

--- srcs/core/networking/libraries/server/events/send.lua
+++ srcs/core/networking/libraries/server/events/send.lua
@@ -1,10 +1,15 @@
 function LIBRARY:Call(tServer, tEvent)
 	local sID		= tostring(tEvent.udPeer:connect_id())
-	local udPeer	= tServer:IsValidClient(sID)
+	local tPeer		= tServer:IsValidClient(sID)
 
-	if not udPeer then
-		return MsgC(Color(231, 76, 60), "Attempted to send message to unregister Client [ID : " .. sID .. "]  : " .. tostring(udPeer))
+	if not tPeer then
+		return MsgC(Color(231, 76, 60), "Attempted to send message to unregister Client [ID : " .. sID .. "]  : " .. tostring(tPeer[1]))
 	end
+
+	local Encoded = tServer.CODEC:Encode(tEvent.Data)
+
+	print(Encoded)
+	print(#Encoded)
 		
-	udPeer:send(tServer.CODEC:Encode(tEvent.Data), tEvent.iChannel, tEvent.sFlag)
+	tPeer[1]:send(Encoded, tEvent.iChannel, tEvent.sFlag)
 end
