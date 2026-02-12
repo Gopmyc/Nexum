6f3b7c6a0355af0e03313fc8e7e34bda7e82a49d
Gopmyc <hoyaux.gaspard@gmail.com>
2026-02-12 23:25:42 +0100

    chore(main.lua): ♻️ - update miscellaneous file ()

--- main.lua
+++ main.lua
@@ -1,19 +1,19 @@
 function lovr.load(tArgs)
-	assert((tArgs[1] == "SERVER") or (tArgs[1] == "CLIENT"), "error: First argument must be either 'SERVER' or 'CLIENT', use : ./ArcNet-SERVER.exe 'SERVER' or : ./ArcNet-SERVER.exe 'CLIENT'")
+	assert((tArgs[1] == "SERVER") or (tArgs[1] == "CLIENT"), "error: First argument must be either 'SERVER' or 'CLIENT', use : ./Nexum.exe 'SERVER' or : ./Nexum.exe 'CLIENT'")
 	SERVER	= (tArgs[1] == "SERVER")
 	CLIENT	= (tArgs[1] == "CLIENT")
 
-	ArcNet = require("srcs")
+	Nexum = require("srcs")
 end
 
 function lovr.update(iDeltaTime)
-	ArcNet:Update(iDeltaTime)
+	Nexum:Update(iDeltaTime)
 end
 
 function lovr.draw(Pass)
-	ArcNet:Draw(Pass)
+	Nexum:Draw(Pass)
 end
 
 function lovr.quit()
-	ArcNet:Quit()
+	Nexum:Quit()
 end
