fc0c0b1e3fb64dd93d85093fb5b18f473366c022
Gopmyc <hoyaux.gaspard@gmail.com>
2026-02-20 16:04:29 +0100

    chore(srcs/core/networking/libraries/client/events/send.lua): 🗑️ - remove core component (send.lua)

--- srcs/core/networking/libraries/client/events/send.lua
+++
@@ -1,5 +0,0 @@
-function LIBRARY:Call(tClient, tEvent)
-	local sID		= tostring(tEvent.udPeer:connect_id())
-		
-	tClient:GetPeer():send(tClient.CODEC:Encode(tEvent.Data), tEvent.iChannel, tEvent.sFlag)
-end
