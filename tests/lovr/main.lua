d475df99b2fd725650edb14935fa9ce83ba72717
Gopmyc <hoyaux.gaspard@gmail.com>
2026-02-22 16:21:43 +0100

    chore(tests/lovr/main.lua): ➕ - add miscellaneous file (main.lua)

---
+++ tests/lovr/main.lua
@@ -0,0 +1,19 @@
+function lovr.load(tArgs)
+	tArgs[1]	= tArgs[1] and string.upper(tArgs[1]) or nil
+	SERVER	= (tArgs[1] == "SERVER")
+	CLIENT	= (tArgs[1] == "CLIENT")
+
+	Nexum = require("srcs")
+end
+
+function lovr.update(iDeltaTime)
+	Nexum:Update(iDeltaTime)
+end
+
+function lovr.draw(Pass)
+	Nexum:Draw(Pass)
+end
+
+function lovr.quit()
+	Nexum:Quit()
+end
