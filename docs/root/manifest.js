1c12b5d317cd3adf5b9a2b05a62fe52c25a2ffe6
Gopmyc <hoyaux.gaspard@gmail.com>
2026-03-01 22:34:07 +0100

    docs(root/manifest.js): 📄 - add manifest JS (manifest.js)

---
+++ docs/root/manifest.js
@@ -0,0 +1,56 @@
+const manifest = {
+		"CORE": {
+				"LOADER": {
+						"CLASS.html": null,
+						"LIBRARIES": {
+								"ENV_BUILDER.html": null,
+								"ENV_LOADER.html": null,
+								"FINDER.html": null,
+								"ORDERER.html": null,
+								"RESSOURCES.html": null,
+								"RUNTIME.html": null
+						},
+						"SUBLOADERS": {
+								"CORE.html": null,
+								"SUBLOADERS_BASE.html": null
+						}
+				}
+		},
+		"ENV": {
+				"Color.html": null,
+				"FilesFind.html": null,
+				"Is": {
+						"IsBool.html": null,
+						"IsDirectory.html": null,
+						"IsFile.html": null,
+						"IsFunction.html": null,
+						"IsNumber.html": null,
+						"IsString.html": null,
+						"IsTable.html": null,
+						"IsThread.html": null,
+						"IsUserdata.html": null
+				},
+				"LoadFileInEnvironment.html": null,
+				"MsgC.html": null,
+				"PrintTable.html": null,
+				"ReadFile.html": null,
+				"string": {
+						"string.Explode.html": null,
+						"string.Totable.html": null
+				},
+				"table": {
+						"table.Add.html": null,
+						"table.Copy.html": null
+				}
+		},
+		"GLOBAL_LIBRARIES": {
+				"BASE64.html": null,
+				"JSON.html": null,
+				"LZW.html": null,
+				"PLC": {
+						"CHACHA20.html": null,
+						"POLY1305.html": null
+				},
+				"YAML.html": null
+		}
+};
