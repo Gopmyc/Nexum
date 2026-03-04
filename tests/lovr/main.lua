6bb8064911f1de4ad56cc6ba5c7d29174d0c1313
Gopmyc <hoyaux.gaspard@gmail.com>
2026-03-04 19:41:17 +0100

    chore(tests/lovr/main.lua): ♻️ - update miscellaneous file (main.lua)

--- tests/lovr/main.lua
+++ tests/lovr/main.lua
@@ -1,13 +1,15 @@
 function lovr.load(tArgs)
-	tArgs[1]	= tArgs[1] and string.upper(tArgs[1]) or nil
-	SERVER	= (tArgs[1] == "SERVER")
-	CLIENT	= (tArgs[1] == "CLIENT")
+	tArgs[1] = tArgs[1] and string.upper(tArgs[1]) or nil
+	SERVER = (tArgs[1] == "SERVER")
+	CLIENT = (tArgs[1] == "CLIENT")
 
 	Nexum = require("srcs")
+	local tRelay = Nexum:Instanciate("relay", "RELAY #1")
+	local tRelay = Nexum:Instanciate("console", "console")
 end
 
-function lovr.update(iDeltaTime)
-	Nexum:Update(iDeltaTime)
+function lovr.update(dt)
+	Nexum:Update(dt)
 end
 
 function lovr.draw(Pass)
